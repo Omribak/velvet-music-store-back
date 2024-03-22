bdeac864d7f418b8e5456b91403c7ef8d6f7b816
Omribak <97708552+Omribak@users.noreply.github.com>
2024-03-22 12:39:19 +0200

    Update UserController.js

--- backend-store/controllers/UserController.js
+++ backend-store/controllers/UserController.js
@@ -103,6 +103,7 @@ exports.Login = async (req, res, next) => {
         const cookieOptions = {
           expires: new Date(Date.now() + 60 * 60 * 1000),
           httpOnly: true,
+          sameSite: "None",
           secure: true
         };
         res.cookie('jwt', token, cookieOptions);
